ef37c2623058fa1bfb63686cea03d450beb671cf
Shaham Hai <shahamh@gmail.com>
2016-09-12 15:17:39 +0300

    try

--- config/env/development.js
+++ config/env/development.js
@@ -4,7 +4,8 @@ var defaultEnvConfig = require('./default');
 
 module.exports = {
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev',
+    uri: 'mongodb://user1:1234@ds017584.mlab.com:17584/mean_project1',
+    //uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev',
     options: {
       user: '',
       pass: ''
