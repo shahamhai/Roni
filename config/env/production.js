2349b79bfcac81454dbdc39d056efce1a8fe539a
Shaham Hai <shahamh@gmail.com>
2016-10-26 16:37:51 +0300

    config production settings

--- config/env/production.js
+++ config/env/production.js
@@ -4,7 +4,8 @@ module.exports = {
   secure: true,
   port: process.env.PORT || 8443,
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
+    uri: 'mongodb://user1:1234@ds035177.mlab.com:35177/roni_production',
+    //uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
     options: {
       user: '',
       pass: ''
