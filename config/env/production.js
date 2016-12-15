ff1268cd1cc27ba016da317fbaf56ecd976892d0
Shaham Hai <shahamh@gmail.com>
2016-12-15 10:39:35 +0200

    change production port

--- config/env/production.js
+++ config/env/production.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   secure: false,
-  port: process.env.PORT || 8443,
+  port: process.env.PORT || 3000,
   db: {
     uri: 'mongodb://user1:1234@ds035177.mlab.com:35177/roni_production',
     //uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
